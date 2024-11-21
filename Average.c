fe93437574e7d3d067e3c8becaa57a6027a34803
Enzo <160965214+Roxlays@users.noreply.github.com>
2024-11-21 11:59:59 +0000

    fonction average

---
+++ Average.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float average(int length, int array[]);
+
+int main(void)
+{
+    int n = 5;
+    int scores[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        scores[i] = i;
+    }
+
+    printf("average : %.lf\n", average(n,scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return (float)sum / length;
+
+}
