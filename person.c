133433bb15ee575336e548b460a89dc7411134b2
Enzo <160965214+Roxlays@users.noreply.github.com>
2024-11-21 14:29:55 +0000

    exo malloc/realloc

---
+++ person.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+struct Person 
+{
+    int id;
+    char c;
+    struct  Person* mother;    
+}; Person;
+
+int main()
+{
+    int number[5] = {1,2,3,4,5};
+    int number[6] = 6;
+}
